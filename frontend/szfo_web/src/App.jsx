36cba0110646399bb1195d577c7e6ddefc79f51f
Andrey <klad785@mail.ru>
2024-10-11 20:54:34 +0300

    comit

---
+++ frontend/szfo_web/src/App.jsx
@@ -0,0 +1,12 @@
+import img from "./lap.jpg"
+
+
+function App() {
+  return (
+    <div className="App">
+      <input type="file" className="fileUpload" multiple="multiple"></input>
+    </div>
+  );
+}
+
+export default App;
